ddc1b98d9af2324f2cd409e22fb8594d7cd64c72
davlatabdullaev <davlatdavlat0206@gmail.com>
2024-02-13 14:31:19 +0500

    updated

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.6
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-playground/assert/v2 v2.2.0
 	github.com/golang-migrate/migrate/v4 v4.17.0
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.5.3
---
+++ storage/postgres/basket_test.go
@@ -0,0 +1,149 @@
+package postgres
+
+import (
+	"context"
+	"test/api/models"
+	"test/config"
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+)
+
+func TestBasketRepo_Create(t *testing.T) {
+
+	cfg := config.Load()
+
+	pgStore, err := New(context.Background(), cfg)
+	if err != nil {
+		t.Errorf("error while connection to db error: %v", err)
+	}
+
+	createBasket := models.CreateBasket{
+		CustomerID: "",
+		TotalSum:   0,
+	}
+
+	basketID, err := pgStore.Basket().Create(context.Background(), createBasket)
+	if err != nil {
+		t.Errorf("error while creating basket error : %v", err)
+	}
+
+	basket, err := pgStore.Basket().GetByID(context.Background(), models.PrimaryKey{
+		ID: basketID,
+	})
+	if err != nil {
+		t.Errorf("error while getting basket error: %v", err)
+	}
+
+	assert.Equal(t, basket.CustomerID, createBasket.CustomerID)
+	assert.Equal(t, basket.TotalSum, createBasket.TotalSum)
+
+}
+
+func TestBasketRepo_GetByID(t *testing.T) {
+
+	cfg := config.Load()
+
+	pgStore, err := New(context.Background(), cfg)
+	if err != nil {
+		t.Errorf("error while connection to db error: %v", err)
+	}
+	createBasket := models.CreateBasket{
+		TotalSum: 0,
+	}
+
+	basketID, err := pgStore.Basket().Create(context.Background(), createBasket)
+	if err != nil {
+		t.Errorf("error while creating basket error: %v", err)
+	}
+
+	basket, err := pgStore.Basket().GetByID(context.Background(), models.PrimaryKey{
+		ID: basketID,
+	})
+	if err != nil {
+		t.Errorf("error while getting primary key : %v", err)
+	}
+
+	assert.Equal(t, basket.ID, basketID)
+
+}
+
+func TestBasketRepo_GetList(t *testing.T) {
+	cfg := config.Load()
+
+	pgStore, err := New(context.Background(), cfg)
+	if err != nil {
+		t.Errorf("error while connection to db error: %v", err)
+	}
+
+	basketResp, err := pgStore.Basket().GetList(context.Background(), models.GetListRequest{
+		Page:  1,
+		Limit: 1000,
+	})
+	if err != nil {
+		t.Errorf("error while getting basketResp error: %v", err)
+	}
+
+	if len(basketResp.Baskets) != 2 {
+		t.Errorf("expected 16, but got: %d", len(basketResp.Baskets))
+	}
+
+	assert.Equal(t, len(basketResp.Baskets), 2)
+
+}
+
+func TestBasketRepo_Update(t *testing.T) {
+	cfg := config.Load()
+
+	pgStore, err := New(context.Background(), cfg)
+	if err != nil {
+		t.Errorf("error while connection to db error: %v", err)
+	}
+	createBasket := models.CreateBasket{
+		CustomerID: "",
+		TotalSum:   0,
+	}
+
+	basketID, err := pgStore.Basket().Create(context.Background(), createBasket)
+	if err != nil {
+		t.Errorf("error while creating basket error: %v", err)
+	}
+
+	updateBasket := models.UpdateBasket{
+		ID:         basketID,
+		CustomerID: "",
+		TotalSum:   0,
+	}
+
+	updateBasketID, err := pgStore.Basket().Update(context.Background(), updateBasket)
+	if err != nil {
+		t.Errorf("error while creating basket error: %v", err)
+	}
+
+	assert.Equal(t, basketID, updateBasketID)
+}
+
+func TestBasketRepo_Delete(t *testing.T) {
+	cfg := config.Load()
+
+	pgStore, err := New(context.Background(), cfg)
+	if err != nil {
+		t.Errorf("error while connection to db error: %v", err)
+	}
+
+	createBasket := models.CreateBasket{
+		CustomerID: "",
+		TotalSum:   0,
+	}
+
+	basketID, err := pgStore.Basket().Create(context.Background(), createBasket)
+	if err != nil {
+		t.Errorf("error while creating basket error: %v", err)
+	}
+
+	if err := pgStore.Basket().Delete(context.Background(), models.PrimaryKey{
+		ID: basketID,
+	}); err != nil {
+		t.Errorf("Error deleting basket: %v", err)
+	}
+}
